e354340e6895bc237bbed745fa9304227668dee1
jethro tang <jthtang10@gmail.com>
2022-05-06 04:27:31 +0100

    fixed url to wordlist

--- config/initializers/load_words.rb
+++ config/initializers/load_words.rb
@@ -1,2 +1,2 @@
 # $words = HTTP.get('https://raw.githubusercontent.com/dwyl/english-words/master/words.txt').to_s.split("\n").map(&:downcase).uniq.freeze
-$words = HTTP.get('https://github.com/lorenbrichter/Words/blob/master/Words/en.txt').to_s.split("\n").map(&:downcase).uniq.freeze
+$words = HTTP.get('https://raw.githubusercontent.com/lorenbrichter/Words/master/Words/en.txt').to_s.split("\n").map(&:downcase).uniq.freeze
